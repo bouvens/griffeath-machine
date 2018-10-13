d330125400308daf2da4b5c13c3438b441cdd5d2
Alexander Demin <bouvens@gmail.com>
2018-10-13 20:38:14 +0600

    feat: Add stats.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import './index.css'
 import GriffeathMachine from './components/GriffeathMachine'
 
 render(<GriffeathMachine />, document.querySelector('#app'))
+
+// eslint-disable-next-line no-undef, comment: it's global
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+requestAnimationFrame(function loop () {
+    stats.update()
+    requestAnimationFrame(loop)
+})
