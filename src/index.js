41f84f3d9ebb51efc932baad67eda01fb852f24f
Alexander Demin <bouvens@gmail.com>
2019-04-21 16:39:14 +0600

    refactor: Transfer the project to Parcel bundler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-
-import './index.css'
 import App from './components/App'
+import Stats from './lib/stats'
+import './index.css'
 
 render(<App />, document.querySelector('#app'))
 
-// eslint-disable-next-line no-undef, comment: it's global
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 requestAnimationFrame(function loop () {
