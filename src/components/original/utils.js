7d928d91f82466d8d973572e9122ddc11a0c4b68
Alexander Demin <bouvens@gmail.com>
2019-05-19 01:25:25 +0600

    refactor: Extract utils of OriginalMachine

---
+++ src/components/original/utils.js
@@ -0,0 +1,33 @@
+export function getRandomField ({ width, height, states }) {
+  const field = []
+
+  for (let x = 0; x < width; x += 1) {
+    field[x] = []
+    for (let y = 0; y < height; y += 1) {
+      field[x][y] = Math.floor(Math.random() * states)
+    }
+  }
+
+  return field
+}
+
+const mod = (number, limit) => (number < 0 ? number + limit : number % limit)
+
+export function getUpdatedElement (element, x, y, field, width, height, states) {
+  const plusOne = mod(element + 1, states)
+
+  if (field[x][mod(y - 1, height)] === plusOne
+    || field[x][mod(y + 1, height)] === plusOne
+    || field[mod(x - 1, width)][y] === plusOne
+    || field[mod(x + 1, width)][y] === plusOne) {
+    return plusOne
+  }
+
+  return element
+}
+
+export const getUpdatedField = ({ field, width, height, states }) => field.map(
+  (column, x) => column.map(
+    (element, y) => getUpdatedElement(element, x, y, field, width, height, states)
+  )
+)
