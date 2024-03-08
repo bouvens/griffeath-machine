6e5662132576eb05386a6fc4ce701570d535a530
Alex Demin <bouvens@gmail.com>
2024-03-08 22:07:15 +0500

    Updates

--- gpu-utils.bundle.js
+++ gpu-utils.bundle.js
@@ -1,14 +1,14 @@
 "use strict";
-(this["webpackChunkgriffeath_machine"] = this["webpackChunkgriffeath_machine"] || []).push([[809],{
+(this["webpackChunkgriffeath_machine"] = this["webpackChunkgriffeath_machine"] || []).push([[415],{
 
-/***/ 588:
+/***/ 985:
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "makeGetUpdatedField": function() { return /* binding */ makeGetUpdatedField; }
+/* harmony export */   makeGetUpdatedField: function() { return /* binding */ makeGetUpdatedField; }
 /* harmony export */ });
-/* harmony import */ var gpu_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(985);
+/* harmony import */ var gpu_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(788);
 /* harmony import */ var gpu_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(gpu_js__WEBPACK_IMPORTED_MODULE_0__);
 
 
@@ -26,11 +26,11 @@ function myMod(number, limit) {
 
 function updateElement(field, width, height, states) {
   // what a mess
-  var ver = this.thread.x;
-  var hor = this.thread.y;
-  var element = field[hor][ver];
-  var plusOne = myMod(element + 1, states);
-  var next = myMod(hor - 1, width);
+  const ver = this.thread.x;
+  const hor = this.thread.y;
+  const element = field[hor][ver];
+  const plusOne = myMod(element + 1, states);
+  let next = myMod(hor - 1, width);
 
   if (field[next][ver] === plusOne) {
     return plusOne;
@@ -57,11 +57,9 @@ function updateElement(field, width, height, states) {
   return element;
 }
 
-var gpu = new gpu_js__WEBPACK_IMPORTED_MODULE_0__.GPU();
-var makeGetUpdatedField = function makeGetUpdatedField(fieldWidth, fieldHeight) {
-  return gpu.createKernel(updateElement).setOutput([fieldHeight, fieldWidth]) // more of the mess
-  .setFunctions([myMod]);
-};
+const gpu = new gpu_js__WEBPACK_IMPORTED_MODULE_0__.GPU();
+const makeGetUpdatedField = (fieldWidth, fieldHeight) => gpu.createKernel(updateElement).setOutput([fieldHeight, fieldWidth]) // more of the mess
+.setFunctions([myMod]);
 
 /***/ })
 
